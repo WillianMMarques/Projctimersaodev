d94891d2d40d96aa0549ec4b80a9984ed57a28e7
WillianMMarques <willmmuniz@gmail.com>
2021-09-30 21:31:39 -0300

    Incio

---
+++ p1/calculadora.js
@@ -0,0 +1,20 @@
+function calcular() {
+
+    var primeiraNota = parseFloat(document.getElementById("primeiraNota").value)
+    var segundaNota = parseFloat(document.getElementById("segundaNota").value)
+    var terceiraNota = parseFloat(document.getElementById("terceiraNota").value)
+    var quartaNota = parseFloat(document.getElementById("quartaNota").value) 
+    
+    
+    var nota = (primeiraNota + segundaNota + terceiraNota + quartaNota)/4
+
+    
+    
+    
+      console.log(nota)
+    
+    
+
+
+
+}
